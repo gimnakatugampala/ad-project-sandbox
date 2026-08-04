069eb6af2982ea1540f4c73b36320e4f4e9d1ca7
gimnakatugampala <gimnakatugampala1@gmail.com>
2026-08-04 18:12:27 +0530

    google_auth_works

--- src/auth.ts
+++ src/auth.ts
@@ -2,8 +2,14 @@ import NextAuth from  'next-auth'
 import {PrismaAdapter} from '@auth/prisma-adapter'
 import {prisma} from '@/lib/prisma'
 
+import Google from 'next-auth/providers/google'
+
 export const { handlers, auth, signIn, signOut } = NextAuth({
     adapter: PrismaAdapter(prisma),
     secret: process.env.AUTH_SECRET,
-    providers: []
+    providers: [ 
+    Google({
+    clientId: process.env.AUTH_GOOGLE_ID!,
+    clientSecret: process.env.AUTH_GOOGLE_SECRET!,
+  }),]
 })
